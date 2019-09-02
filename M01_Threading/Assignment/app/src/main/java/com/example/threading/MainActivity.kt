2474db75dd75099801f1d66bbc2e7ca9b55af00e
Tyler Berrett <btylerberrett@gmail.com>
2019-09-02 14:30:04 -0600

    initial commit part 1 done

---
+++ M01_Threading/Assignment/app/src/main/java/com/example/threading/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.threading
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        fun primes(): Sequence<Long> {
+            var i = 0L
+            return sequence {
+                generateSequence { i++ }
+                    .filter { n -> n > 1 && ((2 until n).none { i -> n % i == 0L }) }
+                    .forEach { yield(it) }
+            }
+        }
+
+
+    }
+}
