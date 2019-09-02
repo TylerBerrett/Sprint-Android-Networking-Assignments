bbfa134525d1a0c004e87745a03bb1720297da22
Tyler Berrett <btylerberrett@gmail.com>
2019-09-02 14:42:09 -0600

    part 2 done

--- M01_Threading/Assignment/app/src/main/java/com/example/threading/MainActivity.kt
+++ M01_Threading/Assignment/app/src/main/java/com/example/threading/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.threading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ProgressBar
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,15 +12,36 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fun primes(): Sequence<Long> {
-            var i = 0L
-            return sequence {
-                generateSequence { i++ }
-                    .filter { n -> n > 1 && ((2 until n).none { i -> n % i == 0L }) }
-                    .forEach { yield(it) }
-            }
-        }
+        prime_number_text.visibility = TextView.GONE
+        toggleProgressBar()
+
+        val primeNumbers = primes().take(16000).joinToString(", ")
+        prime_number_text.text = "Primes: $primeNumbers"
+
+
 
 
     }
+
+
+    fun toggleProgressBar (){
+        if (progressBar.visibility == ProgressBar.VISIBLE){
+            progressBar.visibility = ProgressBar.GONE
+        }
+        else{
+            progressBar.visibility = ProgressBar.VISIBLE
+        }
+    }
+
+
+    fun primes(): Sequence<Long> {
+        var i = 0L
+        return sequence {
+            generateSequence { i++ }
+                .filter { n -> n > 1 && ((2 until n).none { i -> n % i == 0L }) }
+                .forEach { yield(it) }
+        }
+    }
+
+
 }
