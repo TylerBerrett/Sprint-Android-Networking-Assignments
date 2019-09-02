f02cc22c80396bc9c6e49dd70b13178f616c04b9
Tyler Berrett <btylerberrett@gmail.com>
2019-09-02 15:59:23 -0600

    done?

--- M01_Threading/Assignment/app/src/main/java/com/example/threading/MainActivity.kt
+++ M01_Threading/Assignment/app/src/main/java/com/example/threading/MainActivity.kt
@@ -1,28 +1,58 @@
 package com.example.threading
 
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ProgressBar
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+
+    inner class MyAsyncTask : AsyncTask<Unit, Int, String>(){
+        override fun doInBackground(vararg p0: Unit?): String {
+            val primeNumbers = primes().take(16000).joinToString(", ")
+            return primeNumbers
+        }
+
+        override fun onPreExecute() {
+            super.onPreExecute()
+            prime_number_text.visibility = TextView.GONE
+            toggleProgressBar()
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            toggleProgressBar()
+            prime_number_text.visibility = View.VISIBLE
+            prime_number_text.text = "Primes: $result"
+        }
+
+        override fun onCancelled() {
+            super.onCancelled()
+            println("onCancelled")
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        prime_number_text.visibility = TextView.GONE
-        toggleProgressBar()
-
-        val primeNumbers = primes().take(16000).joinToString(", ")
-        prime_number_text.text = "Primes: $primeNumbers"
-
-
-
-
+        MyAsyncTask().execute()
     }
 
+    override fun onStop() {
+        super.onStop()
+        println("onStop")
+        MyAsyncTask().cancel(false)
+    }
+
+
+
+
+    //Functions below
 
     fun toggleProgressBar (){
         if (progressBar.visibility == ProgressBar.VISIBLE){
